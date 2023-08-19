3010d6bc009106cc9df11f7e28c1360e032b7a3c
Mosesomo <moseswamboga4@gmail.com>
2023-08-19 20:43:13 +0300

    folio

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const header = document.querySelector("header");
-
-window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky", this.window.scrollY > 0);
+window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+//handling navigation bar()
 const openmenu = () =>{
     document.getElementById("navigation").style.display="block";
     document.getElementById("open-btn").style.display="none";
     document.getElementById("close-btn").style.display="block";
 };
-
 const closemenu = () =>{
     document.getElementById("navigation").style.display="none";
     document.getElementById("open-btn").style.display="block";
     document.getElementById("close-btn").style.display="none";
 };
+
+/*const list = document.querySelector('ul');
+
+list.addEventListener('click', ()=>{
+    closemenu();
+});*/
+
+//handling typing effects
+let typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Web Designer", "web Developer"],
+    // Strings to be shown in the typewriter effect.
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
